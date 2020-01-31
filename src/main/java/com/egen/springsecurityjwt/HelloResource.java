a32c4ef15cbafe79d8cf180f370431938548445c
Gagan Solur Venkatesh <gagansolurvenkatesh@Gagan-Egens-MacBook-Pro.local>
2020-01-31 11:52:16 -0600

    Initial commits

---
+++ src/main/java/com/egen/springsecurityjwt/HelloResource.java
@@ -0,0 +1,49 @@
+package com.egen.springsecurityjwt;
+
+import com.egen.springsecurityjwt.model.AuthenticationRequest;
+import com.egen.springsecurityjwt.model.AuthenticationResponse;
+import com.egen.springsecurityjwt.service.MyUserDetailsService;
+import com.egen.springsecurityjwt.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloResource {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+    @RequestMapping(value ="/authenticate", method= RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception{
+        try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+        }catch(BadCredentialsException ex){
+            throw new Exception("Incorrect Credentials: ", ex);
+        }
+        final UserDetails userDetails = myUserDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+}
---
+++ src/main/java/com/egen/springsecurityjwt/filters/JwtRequestFilter.java
@@ -0,0 +1,53 @@
+package com.egen.springsecurityjwt.filters;
+
+import com.egen.springsecurityjwt.service.MyUserDetailsService;
+import com.egen.springsecurityjwt.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+
+        final String authorizationHeader = httpServletRequest.getHeader("authorization");
+        String username = null;
+        String jwtToken = null;
+
+        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+            jwtToken = authorizationHeader.substring(7);
+            username = jwtUtil.extractUsername(jwtToken);
+        }
+
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = this.myUserDetailsService.loadUserByUsername(username);
+            if(jwtUtil.validateToken(jwtToken, userDetails)){
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new
+                        UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        filterChain.doFilter(httpServletRequest,httpServletResponse);
+    }
+}
